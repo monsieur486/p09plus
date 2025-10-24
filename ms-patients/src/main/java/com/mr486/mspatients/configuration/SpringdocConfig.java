0df823c62deffe24ad4f94f21014bfd2d4d2d30f
monsieur486 <monsieur486@gmail.com>
2025-10-24 18:03:50 +0200

    reformat code

--- ms-patients/src/main/java/com/mr486/mspatients/configuration/SpringdocConfig.java
+++ ms-patients/src/main/java/com/mr486/mspatients/configuration/SpringdocConfig.java
@@ -3,10 +3,10 @@ package com.mr486.mspatients.configuration;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import java.util.List;
 
 @Configuration
